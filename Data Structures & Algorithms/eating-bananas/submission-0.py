729b33979708f1d2e3ad9e3065c8e7b94c6858c7
Anjan R. Athreya <anjan.athreya@stonybrook.edu>
2026-06-09 20:44:12 -0400

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,32 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        n = len(piles)
+        l = 1
+        r = max(piles)
+        final_k = 0
+
+        while l <= r:
+            k = (l+r) // 2
+
+            currentHours = 0
+            for i in range(len(piles)):
+                currentHours += math.ceil(piles[i] / k)
+            
+            if currentHours <= h:
+                r = k - 1
+                final_k = k
+            else:
+                l = k + 1
+        
+        return final_k
+
+
+        
+
+"""
+binary search for the target where min(bana/hour) is the target
+binary search not on the array this time
+binary search on an "array" from 1 to h
+"""
