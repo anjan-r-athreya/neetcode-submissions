9c42e3f98b503828ee7b1c7b570f0a69c2188f23
Anjan R. Athreya <anjan.athreya@stonybrook.edu>
2026-08-21 13:54:17 -0700

    Update: same-binary-tree - submission-0

--- Data Structures & Algorithms/same-binary-tree/submission-0.py
+++ Data Structures & Algorithms/same-binary-tree/submission-0.py
@@ -7,8 +7,14 @@
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q: return True
-        if not p or not q: return False
-        if p.val != q.val: return False
         
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        def dfs(p: Optional[TreeNode], q: Optional[TreeNode]):
+            if not p and not q: return True
+
+            if not p or not q: return False
+
+            if p.val != q.val: return False
+
+            return dfs(p.left, q.left) and dfs(p.right, q.right)
+
+        return dfs(p, q)
